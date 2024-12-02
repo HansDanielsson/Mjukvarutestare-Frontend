79641c71ab58475c25fbb4f13c32a82cb25911b6
Hans Danielsson <Hans.Ro.Danielsson@gmail.com>
2024-12-02 08:55:29 +0100

    Edit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Fil för attt hantera Express server
 const Express = require('express')
 const bodyParser = require('body-parser')
+const { createUser, getAllUsers } = require('./Database/dbUserDatabase')
 
 const portNr = 5000
 
@@ -17,4 +18,25 @@ application.listen(portNr, () => {
 // Get-request på root-address för att returnera index.html
 application.get('', (req, res) => {
   res.sendFile('./index.html', { root: __dirname })
+})
+
+application.post('/loginuser', (req, res) => {
+  // Denna payload innehåller 2 st attribut, username och password
+  const data = req.body
+  console.log(data)
+
+  // res.sendFile('./loginuser.html', { root: __dirname })
+})
+
+application.post('/registeruser', async (req, res) => {
+  // Denna payload innehåller 2 st attribut, username och password
+  const data = req.body
+
+  // Spara data till databasen
+  // let respUser = await createUser(data.username.trim(), data.password.trim())
+
+  console.log('Ny användare skapad: ', respUser)
+
+  // Return respons till User
+  res.redirect('/')
 })
