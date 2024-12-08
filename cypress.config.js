3e36e9150874a698f0fbf7d78ffa5708a812f508
Hans Danielsson <Hans.Ro.Danielsson@gmail.com>
2024-12-08 16:09:26 +0100

    Cypress Login

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://localhost:5000',
-    setupNodeEvents(on, config) {
+    baseUrl: 'http://localhost:5000',
+    setupNodeEvents (on, config) {
       // implement node event listeners here
     }
   }
--- cypress/e2e/loginFrontend.cy.js
+++ cypress/e2e/loginFrontend.cy.js
@@ -1,5 +1,37 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Tester mot hemsidan', () => {
+  beforeEach(() => {
+    cy.visit('/')   // Öppna hemsidan
+  })
+  it('register User', () => {
+    cy.wait(1000)
+    cy.get('h1').should('contains.text', 'Welcome to Frontend Homepage')
+    cy.wait(1000)
+    cy.get('#username').type('NewUsername')
+    cy.wait(1000)
+    cy.get('#password').type('NewPassword')
+    cy.wait(1000)
+    cy.get('#register').click()
+    cy.wait(1000)
+  })
+
+  it('Loggin User', () => {
+    // Logga in med NewUsername och NewPassword
+    cy.wait(1000)
+    cy.get('#username').type('NewUsername')
+    cy.wait(1000)
+    cy.get('#password').type('NewPassword')
+    cy.wait(1000)
+    cy.get('#login').click()
+    cy.wait(1000)
+    cy.get('h1').should('contains.text', 'Welcome to Vip User Page')
+    cy.wait(1000)
+    // Verifiera att lösenordet visas
+    cy.get('#savedpassword').should('contain.text', 'NewUsername har password: NewPassword')
+    cy.wait(1000)
+    // Logga ut
+    cy.get('#logout').click()
+    cy.wait(1000)
+    // Verifiera att logga ut lyckats
+    cy.get('#login').should('contain.text', 'Logga in')
   })
 })
